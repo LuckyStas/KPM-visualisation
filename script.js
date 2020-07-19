90d2cbaa5dd3cdf4f05a9fdc2faa765138c27481
LuckyStas <kotykhin.s@gmail.com>
2020-07-19 13:06:06 +0300

    [add] steps

--- script.js
+++ script.js
@@ -1,11 +1,61 @@
 const form = document.querySelector(".input-form");
 const visualization = document.querySelector(".visualization");
+const playPause = document.querySelector(".algo-toggle");
+const stepBack = document.querySelector(".left");
+const stepForward = document.querySelector(".right");
+const domObject = document.querySelector(".algo");
 
 
+let length;
+let myHTML;
 let array;
 let subStringLength;
+let ifStopped = false;
+let currentIter = 0;
+let modified = false;
 
-async function prefixFunction() {
+function fun() {
+   return new Promise((resolve) => {
+       let i = 0;
+       const loop = setInterval(() => {
+          i++;
+          if(!ifStopped) {
+            clearInterval(loop);
+            resolve(i)
+          }
+       }, 10)
+   })
+}
+
+async function animate() {
+   for (let index = 0; index < length; index++) {
+      domObject.innerHTML = myHTML[index];  
+      await new Promise((resolve) => setTimeout(() => resolve(), 1000));
+      if (modified) {
+         index = currentIter;
+         modified = false;
+      } else {
+      currentIter = index;
+      }
+      if (ifStopped) {
+         const val = await fun(); 
+      }
+   }
+   document.querySelector(".again").classList.remove("visually-hidden");
+}
+
+function animationArray() {
+   domObject.innerHTML = myHTML[currentIter]; 
+   form.classList.add("visually-hidden");
+   visualization.classList.remove("visually-hidden");
+   playPause.classList.remove("visually-hidden");
+   stepBack.classList.remove("visually-hidden");
+   stepForward.classList.remove("visually-hidden");
+   animate();
+}
+
+
+function prefixFunction() {
    const htmlStr = document.querySelector(".prefix-sums");
    const ifFound = document.querySelector(".found");
    const string = document.querySelector(".search-string");
@@ -13,16 +63,14 @@ async function prefixFunction() {
    let prefixes = Array(array.length);
    for(let i = 0; i < array.length; ++i) {
       prefixes[i] = 0;
+      if(i <= subStringLength) {
+         htmlStr.innerHTML += "<span>-<span>";
+      }
    }
+   myHTML = [domObject.innerHTML.toString()];
    let k = 0;
    let found = 0;
-   for(let i = 1; i < array.length; ++i) {
-      if(i <= subStringLength) {
-         prefixes[i] = 0;
-         htmlStr.innerHTML += "<span>-<span>";
-         continue;
-      }
-      await new Promise((resolve) => setTimeout(() => resolve(), 1000));
+   for(let i = subStringLength + 1; i < array.length; ++i) {
       while (k > 0 && array[k] !== array[i]) {
          k = prefixes[k-1];
       }
@@ -60,8 +108,10 @@ async function prefixFunction() {
       } else {
       htmlStr.innerHTML += "<span>" + prefixes[i] + "<span>";
       }
+      length = myHTML.push(domObject.innerHTML.toString());
    }
-   document.querySelector(".again").classList.remove("visually-hidden");
+   animationArray();
+   
 }
 
 
@@ -72,8 +122,7 @@ function handlerForm(event) {
    if (strTB.value === "" || substrTB.value === "") {
       return;
    } else {
-      form.classList.add("visually-hidden");
-      visualization.classList.remove("visually-hidden");
+
       const string = document.querySelector(".search-string");
       subStringLength = substrTB.value.length;
       const workString = substrTB.value + "#" + strTB.value;
@@ -85,5 +134,35 @@ function handlerForm(event) {
    prefixFunction()
 }
 
+function stopAlgo(event) {
+   event.preventDefault();
+   if (playPause.classList.contains("pause")) {
+      playPause.classList.add("play");
+      playPause.classList.remove("pause");
+      ifStopped = true;
+   } else {
+      playPause.classList.add("pause");
+      playPause.classList.remove("play");
+      ifStopped = false;
+   }
+
+}
+
+function back(event) {
+   event.preventDefault();
+   --currentIter;
+   domObject.innerHTML = myHTML[currentIter];
+   modified = true;
+}
+
+function forward(event) {
+   event.preventDefault();
+   ++currentIter;
+   domObject.innerHTML = myHTML[currentIter];
+   modified = true;
+}
 
 form.addEventListener("submit", handlerForm);
+playPause.addEventListener("click", stopAlgo);
+stepBack.addEventListener("click", back);
+stepForward.addEventListener("click", forward);
